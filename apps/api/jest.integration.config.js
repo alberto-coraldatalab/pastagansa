6d95a4e24ca5bb9248d6d947b56138d8a9e2b409
Alberto Cortinas <257283700+acortinasm@users.noreply.github.com>
2026-09-08 10:04:56 +0200

    fix: discover integration tests in CI

--- apps/api/jest.integration.config.js
+++ apps/api/jest.integration.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   moduleFileExtensions: ["js", "json", "ts"],
   rootDir: ".",
-  testRegex: "^test/.*\\.e2e-spec\\.ts$",
+  testRegex: ".*\\.e2e-spec\\.ts$",
   transform: { "^.+\\.(t|j)s$": ["ts-jest", { tsconfig: "tsconfig.json" }] },
   testEnvironment: "node",
   testTimeout: 30000,
